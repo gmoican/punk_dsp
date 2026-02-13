b3297dacc119bc5c5d3b8bf6422d8c3f12a1d1f9
Guillermo Moñino Cánovas <guillermomcanovas@gmail.com>
2026-02-13 16:49:12 +0100

    Checkpoint - Saving data...

--- dsp/Followers/EnvelopeFollower.cpp
+++ dsp/Followers/EnvelopeFollower.cpp
@@ -11,8 +11,13 @@ namespace punk_dsp
         sampleRate = spec.sampleRate;
         
         // Recalculate time coefficients based on current sample rate
-        attackCoeff = calculateTimeCoeff (10.0f);
-        releaseCoeff = calculateTimeCoeff (100.0f);
+        attackCoeff = calculateTimeCoeff (attackTime);
+        releaseCoeff = calculateTimeCoeff (releaseTime);
+    }
+
+    void EnvelopeFollower::reset()
+    {
+        envelope_lin = 0.0f;
     }
 
     float EnvelopeFollower::calculateTimeCoeff(float time_ms)
@@ -22,23 +27,49 @@ namespace punk_dsp
 
     void EnvelopeFollower::setAttack(float newAttMs)
     {
-        attackCoeff = calculateTimeCoeff(newAttMs);
+        attackTime = std::max(0.1f, newAttMs);
+        attackCoeff = calculateTimeCoeff(attackTime);
     }
 
     void EnvelopeFollower::setRelease(float newRelMs)
     {
-        releaseCoeff = calculateTimeCoeff(newRelMs);
+        releaseTime = std::max(0.1f, newRelMs);
+        releaseCoeff = calculateTimeCoeff(releaseTime);
     }
 
+    void EnvelopeFollower::setPeakDetection(bool newUsePeakDetector)
+    {
+        usePeakDetector = newUsePeakDetector;
+    }
     
-    void EnvelopeFollower::updateEnvelope(float input_lin)
+    float EnvelopeFollower::peakDetection(float input_lin)
     {
         float target = std::abs(input_lin);
         
         float alpha = (target > envelope_lin) ? attackCoeff : releaseCoeff;
         
-        // Exponential smoothing: y[n] = a * y[n-1] + (1-a) * x[n]
-        envelope_lin = (alpha * envelope_lin) + ((1.0f - alpha) * target);
+        return (alpha * envelope_lin) + ((1.0f - alpha) * target);
+    }
+
+    float EnvelopeFollower::rmsDetection(float input_lin)
+    {
+        float target = input_lin * input_lin;
+
+        float alpha = (target > meanSquare) ? attackCoeff : releaseCoeff;
+
+        meanSquare = (alpha * meanSquare) + ((1.0f - alpha) * target);
+
+        return std::sqrt(meanSquare);
+    }
+
+    void EnvelopeFollower::updateEnvelope(float input_lin)
+    {
+        juce::FloatVectorOperations::disableDenormalisedNumberSupport();
+        
+        if (usePeakDetector)
+            envelope_lin = peakDetection(input_lin);
+        else
+            envelope_lin = rmsDetection(input_lin);
     }
     
     float EnvelopeFollower::getEnvelope()
--- dsp/followers/EnvelopeFollower.h
+++ dsp/followers/EnvelopeFollower.h
@@ -11,10 +11,11 @@ namespace punk_dsp
         ~EnvelopeFollower() = default;
 
         void prepare(const juce::dsp::ProcessSpec& spec);
-        // void reset();
+        void reset();
 
         void setAttack(float newAttMs);
         void setRelease(float newRelMs);
+        void setPeakDetection(bool newUsePeakDetector);
         
         float getEnvelope();
         float updateAndReturnEnvelope(float input_lin);
@@ -22,14 +23,22 @@ namespace punk_dsp
     private:
         float calculateTimeCoeff (float time_ms);
         void updateEnvelope (float input_lin);
+        float peakDetection(float input_lin);
+        float rmsDetection(float input_lin);
 
-        // --- Internal State ---
+        // --- Envelope State ---
         float envelope_lin { 0.0f };    // Current envelope value
                                         // Should be confined between 0 and 1
+        float meanSquare   { 0.0f };    // For RMS calculation
         
         // Parameters
         float attackCoeff  { 0.0f };   // Smoothing coefficient (Attack)
         float releaseCoeff { 0.0f };   // Smoothing coefficient (Release)
+        float attackTime   { 1.0f };   // Attack time in ms
+        float releaseTime  { 1.0f };   // Release time in ms
+        
+        bool usePeakDetector = true;   // TRUE = Use peak detection
+                                       // FALSE = Use RMS detection
         
         // Cached values for performance
         float sampleRate { 44100.0f };
