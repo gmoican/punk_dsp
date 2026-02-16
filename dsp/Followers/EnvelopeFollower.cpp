24f0e4d8070041569bbefa30ab3f5a5da1789c87
Guillermo Moñino Cánovas <guillermomcanovas@gmail.com>
2026-02-16 08:25:57 +0100

    Code cleaning

--- dsp/Followers/EnvelopeFollower.cpp
+++ dsp/Followers/EnvelopeFollower.cpp
@@ -17,7 +17,7 @@ namespace punk_dsp
 
     void EnvelopeFollower::reset()
     {
-        envelope_lin = 0.0f;
+        envelope = 0.0f;
     }
 
     float EnvelopeFollower::calculateTimeCoeff(float time_ms)
@@ -37,49 +37,18 @@ namespace punk_dsp
         releaseCoeff = calculateTimeCoeff(releaseTime);
     }
 
-    void EnvelopeFollower::setPeakDetection(bool newUsePeakDetector)
-    {
-        usePeakDetector = newUsePeakDetector;
-    }
-    
-    float EnvelopeFollower::peakDetection(float input_lin)
+    float EnvelopeFollower::process(float input_lin)
     {
         float target = std::abs(input_lin);
         
-        float alpha = (target > envelope_lin) ? attackCoeff : releaseCoeff;
+        float alpha = (target > envelope) ? attackCoeff : releaseCoeff;
+        envelope = alpha * (envelope - target) + target;
         
-        return (alpha * envelope_lin) + ((1.0f - alpha) * target);
-    }
-
-    float EnvelopeFollower::rmsDetection(float input_lin)
-    {
-        float target = input_lin * input_lin;
-
-        float alpha = (target > meanSquare) ? attackCoeff : releaseCoeff;
-
-        meanSquare = (alpha * meanSquare) + ((1.0f - alpha) * target);
-
-        return std::sqrt(meanSquare);
-    }
-
-    void EnvelopeFollower::updateEnvelope(float input_lin)
-    {
-        juce::FloatVectorOperations::disableDenormalisedNumberSupport();
-        
-        if (usePeakDetector)
-            envelope_lin = peakDetection(input_lin);
-        else
-            envelope_lin = rmsDetection(input_lin);
+        return envelope;
     }
     
     float EnvelopeFollower::getEnvelope()
     {
-        return envelope_lin;
-    }
-
-    float EnvelopeFollower::updateAndReturnEnvelope(float input_lin)
-    {
-        updateEnvelope(input_lin);
-        return getEnvelope();
+        return envelope;
     }
 }
--- dsp/followers/EnvelopeFollower.h
+++ dsp/followers/EnvelopeFollower.h
@@ -13,32 +13,25 @@ namespace punk_dsp
         void prepare(const juce::dsp::ProcessSpec& spec);
         void reset();
 
+        void setSampleRate(float newSampleRate);
         void setAttack(float newAttMs);
         void setRelease(float newRelMs);
-        void setPeakDetection(bool newUsePeakDetector);
         
         float getEnvelope();
-        float updateAndReturnEnvelope(float input_lin);
+        float process(float input_lin);
 
     private:
         float calculateTimeCoeff (float time_ms);
-        void updateEnvelope (float input_lin);
-        float peakDetection(float input_lin);
-        float rmsDetection(float input_lin);
 
         // --- Envelope State ---
-        float envelope_lin { 0.0f };    // Current envelope value
-                                        // Should be confined between 0 and 1
-        float meanSquare   { 0.0f };    // For RMS calculation
+        float envelope { 0.0f };    // Current envelope value
+                                    // Should be confined between 0 and 1
         
         // Parameters
         float attackCoeff  { 0.0f };   // Smoothing coefficient (Attack)
         float releaseCoeff { 0.0f };   // Smoothing coefficient (Release)
         float attackTime   { 1.0f };   // Attack time in ms
         float releaseTime  { 1.0f };   // Release time in ms
-        
-        bool usePeakDetector = true;   // TRUE = Use peak detection
-                                       // FALSE = Use RMS detection
         
         // Cached values for performance
         float sampleRate { 44100.0f };
